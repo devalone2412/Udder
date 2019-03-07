48132e56e8dfec885a4cebb79c114d8219f47c24
Luc Thoi Sang <devbrownbear@gmail.com>
2019-03-07 10:52:40 +0700

    Add new function

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         <#code#>
     }
+    
+    override func viewWillLayoutSubviews() {
+        <#code#>
+    }
 
 }
 
